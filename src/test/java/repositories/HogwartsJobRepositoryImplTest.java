1065c8bc2cef9ae59a50990b9050a2f27fb6a213
thaaz <thaazar@gmail.com>
2020-02-13 21:53:26 +0100

    add student and job view on web

--- src/test/java/repositories/HogwartsJobRepositoryImplTest.java
+++ src/test/java/repositories/HogwartsJobRepositoryImplTest.java
@@ -6,6 +6,8 @@ import harryPotterApp.repositories.CharacterRepository;
 import harryPotterApp.repositories.CharacterRepositoryImpl;
 import harryPotterApp.repositories.HogwartsJobRepository;
 import harryPotterApp.repositories.HogwartsJobRepositoryImpl;
+import harryPotterApp.startingData.DataInitializer;
+import harryPotterApp.startingData.SingletonEntityManagerFactory;
 import org.junit.jupiter.api.*;
 
 import javax.persistence.EntityManager;
@@ -22,31 +24,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class HogwartsJobRepositoryImplTest {
 
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("ORM");
-    private EntityManager em;
+    private static EntityManager em;
     private CharacterRepository characterRepository;
     private HogwartsJobRepository hogwartsJobRepository;
-    private HPCharacter albusDumbledore;
-    private HPCharacter severusSnape;
-    private HPCharacter harryPotter;
 
-    private HogwartsJob elixirMaster;
-    private HogwartsJob headmaster;
-    private HogwartsJob houseHead;
-
-
+    @BeforeAll
+    static void createData(){
+        em = SingletonEntityManagerFactory.getEmf().createEntityManager();
+        DataInitializer.addAllData(em);
+    }
     @BeforeEach
     public void setUp() {
-        em = emf.createEntityManager();
+        em = SingletonEntityManagerFactory.getEmf().createEntityManager();
         characterRepository = new CharacterRepositoryImpl(em);
         hogwartsJobRepository = new HogwartsJobRepositoryImpl(em);
-        albusDumbledore = new HPCharacter("Albus", "Dumbledore", LocalDate.of(1881, 6, 18));
-        severusSnape = new HPCharacter("Severus", "Snape", LocalDate.of(1960, 6, 1));
-        harryPotter = new HPCharacter("Harry", "Potter", LocalDate.of(1980, 7, 31));
-
-        elixirMaster =
-        headmaster = new HogwartsJob(BigDecimal.valueOf(15000), "Headmaster of Hogwarts");
-        houseHead = new HogwartsJob(BigDecimal.valueOf(7500), "House Head of Slytherin");
     }
 
     @AfterEach
@@ -54,117 +45,18 @@ class HogwartsJobRepositoryImplTest {
         em.close();
     }
 
+
     @Order(1)
     @Test
-    public void shouldAddThreeHPCharacterToDataBase() {
-        EntityTransaction transaction = em.getTransaction();
-        List<HPCharacter> addedHPCharacters = new ArrayList<>();
+    public void shouldReturnJobByCharacterId(){
+        Long characterId = 2L;
+        List<HogwartsJob> characterJobs = hogwartsJobRepository.findJobByIdCharacter(characterId);
 
-        transaction.begin();
-        addedHPCharacters.add(characterRepository.add(albusDumbledore));
-        addedHPCharacters.add(characterRepository.add(severusSnape));
-        addedHPCharacters.add(characterRepository.add(harryPotter));
-        transaction.commit();
-
-        HPCharacter hpCharacter1 = addedHPCharacters.get(0);
-        HPCharacter hpCharacter2 = addedHPCharacters.get(1);
-        HPCharacter hpCharacter3 = addedHPCharacters.get(2);
-
-        assertThat(addedHPCharacters.size()).isEqualTo(3);
-        assertThat(hpCharacter1.getId()).isGreaterThan(0);
-        assertThat(hpCharacter2.getId()).isGreaterThan(0);
-        assertThat(hpCharacter3.getId()).isGreaterThan(0);
+        assertThat(characterJobs.get(0).getPositionName()).isEqualTo("Director of Hogwarts");
 
     }
 
-    @Order(2)
-    @Test
-    public void shouldAddThreeHogwartsJobsToDataBase() {
-        EntityTransaction transaction = em.getTransaction();
-        List<HogwartsJob> addedHogwartsJobs = new ArrayList<>();
 
-        transaction.begin();
-        addedHogwartsJobs.add(hogwartsJobRepository.add(headmaster));
-        addedHogwartsJobs.add(hogwartsJobRepository.add(elixirMaster));
-        addedHogwartsJobs.add(hogwartsJobRepository.add(houseHead));
-        transaction.commit();
-
-        HogwartsJob hogwartsJob1 = addedHogwartsJobs.get(0);
-        HogwartsJob hogwartsJob2 = addedHogwartsJobs.get(1);
-        HogwartsJob hogwartsJob3 = addedHogwartsJobs.get(2);
-
-        assertThat(addedHogwartsJobs.size()).isEqualTo(3);
-        assertThat(hogwartsJob1.getId()).isGreaterThan(0);
-        assertThat(hogwartsJob2.getId()).isGreaterThan(0);
-        assertThat(hogwartsJob3.getId()).isGreaterThan(0);
-    }
-
-
-    @Order(3)
-    @Test
-    public void shouldCreateLinkBetweenOneCharacterAndOneHogwartsJob() {
-        EntityTransaction transaction = em.getTransaction();
-        Long idJobToLink = 2L;
-        Long idCharacterToLink = 2L;
-        HogwartsJob hogwartsJobToLink = hogwartsJobRepository.findById(idJobToLink);
-        HPCharacter characterToLink = characterRepository.findById(idCharacterToLink);
-
-        hogwartsJobToLink.setCharactersAsEmployee(characterToLink);
-        transaction.begin();
-        hogwartsJobRepository.updateById(idJobToLink);
-        transaction.commit();
-
-        HogwartsJob hogwartsJobAfterUpdate = hogwartsJobRepository.findById(idJobToLink);
-        assertThat(hogwartsJobAfterUpdate.getCharactersInJob().size()).isEqualTo(1);
-    }
-
-    @Order(4)
-    @Test
-    public void shouldAddSecondJobToCharacter() {
-        EntityTransaction transaction = em.getTransaction();
-        Long idJobToLink = 3L;
-        Long idCharacterToLink = 2L;
-        HogwartsJob hogwartsJobToLink = hogwartsJobRepository.findById(idJobToLink);
-        HPCharacter characterToLink = characterRepository.findById(idCharacterToLink);
-
-        hogwartsJobToLink.setCharactersAsEmployee(characterToLink);
-        transaction.begin();
-        hogwartsJobRepository.updateById(idJobToLink);
-        characterRepository.update(characterToLink);
-        transaction.commit();
-
-        HPCharacter characterAfterUpdate = characterRepository.findById(idCharacterToLink);
-        assertThat(characterAfterUpdate.getPositions().size()).isEqualTo(2);
-    }
-
-
-    @Order(5)
-    @Test
-    public void shouldChangeSalaryToNewValue(){
-        EntityTransaction transaction = em.getTransaction();
-        Long idJobToChange = 1L;
-        HogwartsJob hogwartsJobToUpdate = hogwartsJobRepository.findById(idJobToChange);
-
-        hogwartsJobToUpdate.setSalary(BigDecimal.valueOf(6666));
-        transaction.begin();
-        hogwartsJobRepository.updateById(idJobToChange);
-        transaction.commit();
-
-        HogwartsJob hogwartsJobAfterSalaryChange = hogwartsJobRepository.findById(idJobToChange);
-        assertThat(hogwartsJobAfterSalaryChange.getSalary()).isEqualTo(BigDecimal.valueOf(6666));
-    }
-
-    //todo
-    /*@Order(6)
-    @Test
-    public void shouldReturnJobByName(){
-        EntityTransaction transaction = em.getTransaction();
-        String jobName = "Elixir Master";
-        HogwartsJob foundedJob = hogwartsJobRepository.findJobByName(jobName);
-
-        assertThat(foundedJob.getPositionName()).isEqualTo(jobName);
-
-    }*/
     @Order(10)
     @Test
     public void shouldDeleteJob(){
