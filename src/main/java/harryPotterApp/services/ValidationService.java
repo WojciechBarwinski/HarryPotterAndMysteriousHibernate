cd468c000489131ceb11080d2aef8910f0cc7d05
Maja Barwińska <majakempa1@gmail.com>
2020-02-14 12:53:07 +0100

    fixed validateSearch method in ValidationService

--- src/main/java/harryPotterApp/services/ValidationService.java
+++ src/main/java/harryPotterApp/services/ValidationService.java
@@ -10,13 +10,18 @@ public class ValidationService {
     private HpCharacterService hpCharacterService = new HpCharacterServiceImpl();
 
     public Map<String, String> validateSearch(String userInput) {
-        List<HPCharacterDto> allCharacters = hpCharacterService.getAllCharacters();
+
         Map<String, String> errorsMap = new HashMap<>();
         if (userInput.isBlank()) {
             errorsMap.put("noId", "You have not entered any Id");
-        } else if (Long.parseLong(userInput) < 1 || Long.parseLong(userInput) > allCharacters.size()){
+        } else if (!isIdPresent(userInput)) {
             errorsMap.put("invalidId", "Searched Id doesn't exist");
         }
         return errorsMap;
+    }
+
+    boolean isIdPresent(String id) {
+        List<HPCharacterDto> allCharacters = hpCharacterService.getAllCharacters();
+        return allCharacters.stream().map(HPCharacterDto::getId).anyMatch(x -> x.equals(Long.valueOf(id)));
     }
 }
